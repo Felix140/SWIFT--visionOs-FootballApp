5d6241edcdb0d84395f7b75ae8568ddc351fb8eb
Felix Valdez <fdvaldez14@gmail.com>
2023-10-24 19:19:25 +0200

    Add File Project

---
+++ FootballApp/FootballAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballAppApp.swift
+//  FootballApp
+//
+//  Created by Felix Valdez on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FootballAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
